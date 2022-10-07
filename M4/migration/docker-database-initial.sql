5d83d7161487295ea6171345c34b559406fbdf57
Bruna Lika <brunalika@msn.com>
2022-10-07 14:53:39 -0300

    postgres docker

---
+++ M4/migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES
+('Bruna', 'Lika'),
+('Nome', 'Historia');
