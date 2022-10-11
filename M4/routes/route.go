2320f8a7b6f9eb110e15a8ed35ab47ca48773e72
Bruna Lika <brunalika@msn.com>
2022-10-11 15:01:28 -0300

    Update route.go

--- M4/routes/route.go
+++ M4/routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"Golang/M4/controllers"
+	"Golang/M4/middleware"
 	"log"
 	"net/http"
 
@@ -10,6 +11,7 @@ import (
 
 func HandleResquest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.GetAllPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.GetByIdPersonalidades).Methods("Get")
